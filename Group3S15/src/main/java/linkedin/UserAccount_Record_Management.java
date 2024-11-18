c17baea731ab89b2043aad8d0925d14d8f17f597
Jyohuuuu <157949020+Jyohuuuu@users.noreply.github.com>
2024-11-18 23:30:08 +0800

    added packages (empty)

---
+++ Group3S15/src/main/java/linkedin/UserAccount_Record_Management.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package linkedin;
+import java.time.LocalDate;
+/**
+ *
+ * @author julia
+ */
+public class UserAccount_Record_Management {
+    public String    accountID;
+    public String    firstName;
+    public String    lastName;
+    public String    contactInfo;
+    public String    email;
+    public String    homeAddress;
+    public LocalDate      birthday;
+    public String    skills;
+    public String    education;
+    public String    employmentHistory;
+    public String    primaryLanguage;
+    public String    secondaryLanguage;
+    
+}

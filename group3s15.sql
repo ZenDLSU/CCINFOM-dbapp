ce28826bccd37d3a3f307e65fa61a3d5389cdf82
Jyohuuuu <157949020+Jyohuuuu@users.noreply.github.com>
2024-11-20 19:31:44 +0800

    Update group3s15.sql fixed

--- group3s15.sql
+++ group3s15.sql
@@ -2,29 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `group3s15`;
 USE `group3s15`;
 
--- User Account Record Management Table
-DROP TABLE IF EXISTS `UserAccount_Record_Management`;
-CREATE TABLE `UserAccount_Record_Management` (
-  `account_ID` DECIMAL(10,0) NOT NULL,
-  `first_name` VARCHAR(50) NOT NULL,
-  `last_name` VARCHAR(50) NOT NULL,
-  `contact_no` TEXT DEFAULT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `home_address` TEXT DEFAULT NULL,
-  `birthday` DATE DEFAULT NULL,
-  `education` TEXT DEFAULT NULL,
-  `years_of_experience` TEXT DEFAULT NULL,
-  `primary_language` TEXT NOT NULL,
-  `secondary_language` TEXT DEFAULT NULL,
-  `job_ID` DECIMAL(10,0) DEFAULT NULL,
-  `company_ID` DECIMAL(10,0) DEFAULT NULL,
-  PRIMARY KEY (`account_ID`),
-  UNIQUE KEY `UA_email_UK` (`email`),
-  KEY `UA_name_IX` (`last_name`, `first_name`),
-  CONSTRAINT `fk_job_ID` FOREIGN KEY (`job_ID`) REFERENCES `Job_Record_Management` (`job_ID`) ON DELETE SET NULL,
-  CONSTRAINT `fk_company_ID` FOREIGN KEY (`company_ID`) REFERENCES `Company_Record_Management` (`company_ID`) ON DELETE SET NULL
-);
-
 -- Company Record Management Table
 DROP TABLE IF EXISTS `Company_Record_Management`;
 CREATE TABLE `Company_Record_Management` (
@@ -63,6 +40,30 @@ CREATE TABLE `Job_Record_Management` (
   CONSTRAINT `fk_job_company_ID` FOREIGN KEY (`company_ID`) REFERENCES `Company_Record_Management` (`company_ID`) ON DELETE SET NULL,
   CONSTRAINT `fk_job_branch_ID` FOREIGN KEY (`branch_ID`) REFERENCES `Branch_Record_Management` (`branch_ID`) ON DELETE SET NULL
 );
+
+-- User Account Record Management Table
+DROP TABLE IF EXISTS `UserAccount_Record_Management`;
+CREATE TABLE `UserAccount_Record_Management` (
+  `account_ID` DECIMAL(10,0) NOT NULL,
+  `first_name` VARCHAR(50) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
+  `contact_no` TEXT DEFAULT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `home_address` TEXT DEFAULT NULL,
+  `birthday` DATE DEFAULT NULL,
+  `education` TEXT DEFAULT NULL,
+  `years_of_experience` TEXT DEFAULT NULL,
+  `primary_language` TEXT NOT NULL,
+  `secondary_language` TEXT DEFAULT NULL,
+  `job_ID` DECIMAL(10,0) DEFAULT NULL,
+  `company_ID` DECIMAL(10,0) DEFAULT NULL,
+  PRIMARY KEY (`account_ID`),
+  UNIQUE KEY `UA_email_UK` (`email`),
+  KEY `UA_name_IX` (`last_name`, `first_name`),
+  CONSTRAINT `fk_job_ID` FOREIGN KEY (`job_ID`) REFERENCES `Job_Record_Management` (`job_ID`) ON DELETE SET NULL,
+  CONSTRAINT `fk_company_ID` FOREIGN KEY (`company_ID`) REFERENCES `Company_Record_Management` (`company_ID`) ON DELETE SET NULL
+);
+
 
 -- Insert into Company Record Management
 INSERT INTO `Company_Record_Management` (company_ID, company_name, contact_no, company_manager, main_location) 

3aab6f1474bd24d616a716736819793466a13b49
ZenDLSU <160311924+ZenDLSU@users.noreply.github.com>
2024-11-24 20:16:28 +0800

    Update Job_Record_Management.java

--- Linkedout/CCINFOM-dbapp-main/Linkedin/src/main/java/link/Job_Record_Management.java
+++ Linkedout/CCINFOM-dbapp-main/Linkedin/src/main/java/link/Job_Record_Management.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+//Author: zen
+
 package link;
 
 import java.sql.Connection;

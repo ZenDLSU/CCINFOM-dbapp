95db7b405f3d02335c05187013dc3647e6c152ff
ZenDLSU <160311924+ZenDLSU@users.noreply.github.com>
2024-11-24 11:48:03 +0800

    Update JobApplicationManagement.java

--- Linkedout/CCINFOM-dbapp-main/Linkedin/src/main/java/link/JobApplicationManagement.java
+++ Linkedout/CCINFOM-dbapp-main/Linkedin/src/main/java/link/JobApplicationManagement.java
@@ -1,15 +1,18 @@
-import java.io.IOException;
+package link;
+
+import your.package.name.DatabaseConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
+
 
 @WebServlet("/JobApplicationManagement")
 public class JobApplicationManagement extends HttpServlet {
@@ -35,4 +38,3 @@ public class JobApplicationManagement extends HttpServlet {
         }
         return jobs;
     }
-}
